2dd132e241c396890a7a51ccc345b057fa553707
azharshaikh7898 <azharshaikh8120.shaikh@gmail.com>
2026-08-14 15:12:24 +0100

    Remove ignored dependencies and build files

--- server/dist/roomManager.d.ts
+++
@@ -1,7 +0,0 @@
-import { Room } from "./room.js";
-export declare class RoomManager {
-    private rooms;
-    getOrCreate(roomId: string): Room;
-    removeIfEmpty(roomId: string): void;
-}
-//# sourceMappingURL=roomManager.d.ts.map
