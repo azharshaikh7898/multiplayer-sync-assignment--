8f511d9338ea5900f74dae3858ccdc054497223f
azharshaikh7898 <azharshaikh8120.shaikh@gmail.com>
2026-08-14 19:58:51 +0100

    Update server PORT to read from env var with 8080 fallback

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import { randomUUID } from "node:crypto";
 import { parseClientMessage } from "./protocol.js";
 import { RoomManager } from "./roomManager.js";
 
-const PORT = 8080;
+const PORT = Number(process.env.PORT) || 8080;
 const HEARTBEAT_INTERVAL_MS = 15000;
 
 const roomManager = new RoomManager();
